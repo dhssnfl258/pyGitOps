150b6f41f0fe842e7fd80c671fd0fc24217243ab
zv322 <dhssnfl258@naver.com>
2024-08-05 17:40:59 +0900

    Resolve merge conflicts!

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ set -e
 # 프로젝트 디렉토리 설정 (예시로 현재 디렉토리 사용)
 PROJECT_DIR=$(pwd)
 
-# 가상 환경 설정 (가상 환경이 필요하지 않은 경우 이 부분은 생략 가능)
-VENV_DIR="$PROJECT_DIR/venv"
-python3 -m venv $VENV_DIR
+# # 가상 환경 설정 (가상 환경이 필요하지 않은 경우 이 부분은 생략 가능)
+# VENV_DIR="$PROJECT_DIR/venv"
+# python3 -m venv $VENV_DIR
 
-# 가상 환경 활성화
-source $VENV_DIR/bin/activate
+# # 가상 환경 활성화
+# source $VENV_DIR/bin/activate
 
 # 필요한 외부 라이브러 리 설치
 pip install -r requirements.txt
